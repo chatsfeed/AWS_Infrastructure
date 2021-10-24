a033d66e629771dc992a2c53eb3a5faa7d734489
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-24 01:13:46 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,11 @@
-provider "aws" {
+#provider "aws" {
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  #region     = "${var.aws_region}"
+#}
+
+provider "aws" {
+   region  = var.aws_region
+   access_key = var.access_key
+   secret_key = var.secret_key
 }
